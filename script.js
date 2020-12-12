d79ca0531ce1d0408b75a0a6d9f3a0bdcc099db9
BrianMbugua5693 <brianmbugua5693@gmail.com>
2020-12-12 11:15:02 +0300

    if else if statement

--- script.js
+++ script.js
@@ -29,3 +29,54 @@ function validate(){
         return false;
     }
 }
+
+
+function claculateDayValue(){
+    year = document.getElementById("year").value;
+    CC = parseInt(year.substring(0,2));
+    YY = parseInt(year.substring(2,4));
+    MM = parseInt(document.getElementById("month").value);
+    DD = parseInt(document.getElementById("date").value);
+    d = ( ( (CC/4) -2*CC - 1) + ( (5*YY/4) ) + ((26*(MM+1)/10) + DD)%7;
+    console.log(d);
+    return (Math.floor(d));
+}
+
+
+function getGender(){
+    var genders = document.getElementsByName("gender");
+    if (genders[0].checked == true){
+        var gender = "male";
+    }
+    else if (genders[1].checked == true){
+        var gender = "female";
+    }
+    else {
+        return false;
+    }
+    switch(gender){
+        case "male":
+            if (dayValue == 1){
+                alert("You were born on " +day[0] + " and Your akan name is " +male[0]+".");
+            }
+            else if(dayValue == -0){
+                alert("You were born on "+day[6] + " and Your akan name is " +male[6]+"!");
+            }
+        break;
+        case "female":
+            if ( dayValue == 1){
+                alert("You were born on "+day[0] + " and Your akan name is  " +female[0]+"."); 
+            }
+            else if(dayValue == 6){
+                alert("You were born on " +day[5] + " and Your akan name is " + female[5]+"!")
+            }
+        break
+        default:
+
+    }
+}
+
+function findName(){
+    dayValue = calculateDayValue();
+    getGender();
+}
